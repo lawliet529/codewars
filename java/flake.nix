3c191206b4e48873f77257e746a52e85bc26e56a
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-04-17 19:07:07 +0700

    Reformat flake.nix

--- java/flake.nix
+++ java/flake.nix
@@ -13,16 +13,14 @@
           maven = prev.maven.override { inherit jdk; };
         })
       ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
+    in {
       devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ gradle jdk maven ];
-        };
+        default = pkgs.mkShell { packages = with pkgs; [ gradle jdk maven ]; };
       });
     };
 }
